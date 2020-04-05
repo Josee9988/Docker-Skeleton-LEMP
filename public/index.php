e7d9373d4c9092b426fd94db3f15084760cc11d8
Josee9988 <jgracia9988@gmail.com>
2020-04-05 16:37:19 +0200

    "information messages in the index.php"

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
 <?php
-
+echo '<h1 style="text-align: center">Success!</h1>';
+echo '<h2 style="text-align: center">You can now remove the "/public/" folder and replace it with your own public directory</h2>';
+echo '<h3 style="text-align: center">The project is now ready for serving your applications such as Symfony, Laravel or any other!</h3>';
+echo '<h4 style="text-align: center">For more information please take a deep look at the <b><a href="https://github.com/Josee9988/Docker-Skeleton-LEMP-PhpMyAdmin">README.md</a></b></h4>';
 phpinfo();
